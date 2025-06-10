8a5c50c0accc8bcd90174507eac0e233c931c169
Nada Ala-Awadi <alawadinada37@gmail.com>
2025-06-11 02:55:54 +0300

    Add files via upload

---
+++ Sanad-Dashboard/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Android\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\sanad1\\sanad_dashboared" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Android\\flutter"
+  "PROJECT_DIR=E:\\sanad1\\sanad_dashboared"
+  "FLUTTER_ROOT=C:\\Android\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\sanad1\\sanad_dashboared\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\sanad1\\sanad_dashboared"
+  "FLUTTER_TARGET=E:\\sanad1\\sanad_dashboared\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\sanad1\\sanad_dashboared\\.dart_tool\\package_config.json"
+)
